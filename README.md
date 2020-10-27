422591af615573336969d4a2104eb13ee6bca4d2
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2020-10-27 15:00:40 -0600

    add forloop to append entry to local storage

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Daily_Planner
 
-A daily planner that includes the date and time.
+This is an interactive daily planner. Tasks for a particular hour will be saved to manage time efficiently. The hours will change color, depending on time of day.
 
 <!-- how to
 1. create the container
 2. consistently update day and time
 3. -->
 
+Resources:
 bootstrap
+
+Screenshot:
+<img src="#" width=500width>
--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 // Current Date and Time
 var timeBlock = document.querySelector('.container')
 
+// var date = new Date();
 
-
-
-
-
-var date = new Date();
+var date = moment().format('MMMM Do YYYY, h:mm:ss a');
 // while loop to append every 1000ms?
 // how to display current time without refresh?
 
 
-var time = ["9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7"]
+var time = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"]
 
 document.getElementById("currentDay").innerHTML = date;
-
-// varibles
-
 
 
 // i want to make a statement that 
@@ -25,40 +19,51 @@ document.getElementById("currentDay").innerHTML = date;
 // for loop to create each timeblock
 for (var i = 0; i < time.length; i++) {
     var timeDiv = document.createElement("div")
+    timeDiv.classList.add('row')
     var timeblockDiv = document.createElement("div")
+    timeblockDiv.classList.add('col-lg-3')
     var textArea = document.createElement("textarea")
+    textArea.classList.add("col-lg-6")
     var button = document.createElement("button")
+    button.classList.add('col-lg-3')
 
     timeblockDiv.textContent = time[i]
+    timeblockDiv.classList.add("hour")
 
-    timeblockDiv.classList.add("row", "time-block")
-
-    timeDiv.appendChild(timeblockDiv)
-
-    textArea.classList.add(textArea)
-
+    // timeDiv.appendChild(timeblockDiv)
+    
     timeblockDiv.appendChild(textArea)
 
     button.innerHTML = "save"
-
     button.classList.add("saveBtn")
+    button.setAttribute("id", time[i])
 
     timeblockDiv.appendChild(button)
 
     timeDiv.classList.add("row")
 
     timeBlock.appendChild(timeblockDiv)
+
+    timeblockDiv.classList.add("row", "time-block")
  }
 
+ document.querySelector(".saveBtn").addEventListener("click", function(event){
+    console.log(event)
+ })
+ 
+ var saveButtons = document.querySelectorAll(".saveBtn")
 
-// // if statement for color change depending on time
-//   if () {
+ for(var i = 0; i < saveButtons.length; i++) {
+   (saveButtons[i]).addEventListener("click", function(event){
+       console.log(event.target.id)
+       console.log(event.target.previousSibling.value)
+       localStorage.setItem(event.target.id, event.target.previousSibling.value)
+        })}
 
-
-//   } else if {
-
-
-//   } else {
-
-
-//   }
+    //    page loads, start first forloop 9-7
+    // insdie use localstorage.getitem = key of item
+    // i+ am/pm
+    // or remove am/pm
+    // @39
+    // moment() if current time < array time (IF STATEMENT)
+    // apply class to element = textArea
